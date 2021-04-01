bc1ae2a3869b3a9a5319483acfbbfc7db1cb6227
winniechae1 <kwakseok1034@gmail.com>
2021-04-01 23:28:56 +0900

    0401 commit

---
+++ jsClass/WebContent/js/day01/test01.js
@@ -0,0 +1,9 @@
+
+
+var no1 = 10;
+console.log(typeof no1);
+
+no1 = 'abcd';
+console.log(typeof no1);
+
+alert("num : " + num);
---
+++ jsClass/WebContent/js/day01/test03.js
@@ -0,0 +1,3 @@
+var age = 31;
+document.write('<div class="w3-red w3-center w3-padding" style="font-size: 24pt;">외부 자바스크립트 파일</div>');
+document.write('내가 부러운 나이는 ' + age + ' 입니다.');
